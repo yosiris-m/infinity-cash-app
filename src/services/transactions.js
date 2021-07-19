106a6cbbda4313022fe3535b4d48dac2821976ca
Yosiris Mariñez <yosiris0510@gmail.com>
2021-07-19 21:40:52 +0200

    get base url depending on environment

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,5 +1,10 @@
+const prodUrl = "https://family-cash-api.herokuapp.com/api/v1";
+const localUrl = "http://localhost:8080/api/v1";
+
+const baseUrl = process.env.NODE_ENV === "production" ? prodUrl : localUrl;
+
 export function getTransactions({ from, to }) {
-  const url = new URL("http://localhost:8080/api/v1/transaction");
+  const url = new URL(`${baseUrl}/transaction`);
   url.search = new URLSearchParams({ from, to }).toString();
 
   return fetch(url).then((res) => res.json());
