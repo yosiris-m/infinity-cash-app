ce281d00f8d8810f97e16e76d21892f5750618d3
Yosiris Mariñez <yosiris0510@gmail.com>
2021-10-18 16:40:57 +0200

    add spinner to loading

--- src/App.js
+++ src/App.js
@@ -11,24 +11,28 @@ import Home from "./pages/home/Home";
 
 import AddTransaction from "./pages/add-transaction/AddTransaction";
 import AddCategory from "./pages/add-category/AddCategory";
+import { Spinner } from "react-bootstrap";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/add-transaction/:type">
-            <AddTransaction />
-          </Route>
-          <Route path="/add-category">
-            <AddCategory />
-          </Route>
-          <Redirect to="/home" />
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <Spinner animation="" color="success" />
+        <div>
+          <Switch>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+            <Route exact path="/add-transaction/:type">
+              <AddTransaction />
+            </Route>
+            <Route path="/add-category">
+              <AddCategory />
+            </Route>
+            <Redirect to="/home" />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
